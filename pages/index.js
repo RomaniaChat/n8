0e65b36869f599de183631093c2453c32bafc657
RomaniaChat <34324435+RomaniaChat@users.noreply.github.com>
2021-01-27 12:12:42 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,7 @@ import Twitter from '../components/icons/twitter';
 import LinkedIn from '../components/icons/linkedin';
 import Instagram from '../components/icons/instagram';
 
-const picUrl = gravatar('RomaniaChat
-', { s: 500, protocol: 'https' });
+const picUrl = gravatar('romaniachat', { s: 500, protocol: 'https' });
 
 export default props => {
   return (
